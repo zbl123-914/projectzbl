f81112ad9ff2124cf74bdd2387672777e5680841
momo-siu <13530305314@163.com>
2024-12-24 13:50:39 +0800

    Update usernamewindow.h

--- usernamewindow.h
+++ usernamewindow.h
@@ -2,24 +2,32 @@
 #define USERNAMEWINDOW_H
 
 #include <QMainWindow>
+#include <QSqlDatabase>
+#include <QSqlQuery>
 
-namespace Ui {
-class usernameWindow;
-}
+QT_BEGIN_NAMESPACE
+namespace Ui { class usernameWindow; }
+QT_END_NAMESPACE
 
 class usernameWindow : public QMainWindow
 {
     Q_OBJECT
 
 public:
-    explicit usernameWindow(QWidget *parent = nullptr);
+    usernameWindow(QWidget *parent = nullptr);
     ~usernameWindow();
 
 private slots:
-    void on_pushButton_clicked();
+    void on_pushButton_clicked();  // 登录按钮槽函数
+    void on_pushButton_2_clicked();  // 注册按钮槽函数
 
 private:
     Ui::usernameWindow *ui;
+    QSqlDatabase db;
+    void openDatabase();
+    void registerUser(const QString &name, const QString &pwd);
+    bool checkUserExists(const QString &name);
+    bool loginUser(const QString &name, const QString &pwd);
 };
 
 #endif // USERNAMEWINDOW_H
